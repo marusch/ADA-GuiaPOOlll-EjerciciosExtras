4161150fd2738dad3fe94c21de7ba8c5bf88243f
Maru Schuindt <maruschuindt07@gmail.com>
2023-02-08 19:40:05 -0300

    Menu - Bucle While

---
+++ Ej3 - Menu/src/Main.java
@@ -0,0 +1,13 @@
+import entidades.Automoviles;
+import entidades.Menu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Menu menuConcesionaria = new Menu();
+
+        menuConcesionaria.menu();
+    }
+}
